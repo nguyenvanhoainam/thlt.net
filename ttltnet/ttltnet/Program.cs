296b33368cb3c6fd2d24a86e814503597c684587
nguyenvanhoainam <namnguyen16102003@gmail.com>
2024-11-15 11:35:18 +0700

    Add files via upload

--- ttltnet/ttltnet/Program.cs
+++ ttltnet/ttltnet/Program.cs
@@ -16,7 +16,7 @@ namespace ttltnet
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new hoadonnhap());
         }
     }
 }
